5e0a33dcf4cb71261235eb6bed0fabcba6f86c89
francisnpls <francis.busano0101@gmail.com>
2023-11-20 19:22:21 +0800

    third commit

---
+++ src/sample/index.js
@@ -0,0 +1,61 @@
+import amira from './amira.jpg';
+import beach1 from './beach1.jpg';
+import beach2 from './beach2.jpg';
+import bgHero from './bgHero.jpg';
+import blckCat from './blckCat.jpeg';
+import blckChild from './blckChild.jpg';
+import blckMoon from './blckMoon.jpg';
+import blckStreet from './blckStreet.jpg';
+import blckWave from './blckWave.jpeg';
+import butterfly from './butterfly.png';
+import camping from './camping.jpg';
+import close from './close.png';
+import flower from './flower.png';
+import heart from './heart.png';
+import IconIG from './IconIG.png';
+import ig1 from './ig1.jpg';
+import ig2 from './ig2.jpg';
+import ig3 from './ig3.jpg';
+import ig4 from './ig4.jpg';
+import ig5 from './ig5.jpg';
+import ig6 from './ig6.jpg';
+import logo from './logo.png';
+import manila from './manila.jpg';
+import menu from './menu.png';
+import mountain from './mountain.jpg';
+import nature from './nature.jpg';
+import pet from './pet.jpg';
+import river from './river.jpg';
+import street from './street.jpg';
+
+export {
+    amira,
+    beach1,
+    beach2,
+    bgHero,
+    blckCat,
+    blckChild,
+    blckMoon,
+    blckStreet,
+    blckWave,
+    butterfly,
+    camping,
+    close,
+    flower,
+    heart,
+    IconIG,
+    ig1,
+    ig2,
+    ig3,
+    ig4,
+    ig5,
+    ig6,
+    logo,
+    manila,
+    menu,
+    mountain,
+    nature,
+    pet,
+    river,
+    street,
+};
